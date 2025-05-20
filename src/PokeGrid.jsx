ea05b99aa0e1ff47b3ab2bfbd90663e6311a8651
kemiilysilva24 <rm95211@estudante.fieb.edu.br>
2025-05-20 11:26:53 +0000

    flex

--- src/PokeGrid.jsx
+++ src/PokeGrid.jsx
@@ -1,9 +1,8 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
-import './Home.css'
+import './PokeGrid.css'
 
-
-const Home = () => {
+const PokeFlex= () => {
 
     return (
         <div>
@@ -20,4 +19,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default PokeGrid
