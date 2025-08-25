7107881071c15f755bde2c11e344baae4b0042c8
kemiillysilva24 <rm95211@estudante.fieb.edu.br>
2025-08-25 11:06:26 -0300

    menu

---
+++ src/Menu.jsx
@@ -0,0 +1,49 @@
+const Menu = () =>{
+
+    return (
+        <div>
+             <ul className= "nav">
+                <li className="nav-item">
+                    <a classe= "nav-link acitive" href ="#">
+                        Active
+                    </a>
+                </li>
+                <li className= "nav-item">
+                    <a className= "nav-link" href="/home">
+                        Home
+                    </a>
+                </li>
+                <li className= "nav-item">
+                <a className="nav-link" href= "/viacep">
+                    ViaCep
+                </a>
+                </li>
+                
+                
+                
+                
+                
+                
+                </ul>   
+
+
+
+
+        </div>    
+
+
+
+
+
+
+
+
+    )
+
+
+
+
+
+}
+
+export default Menu 
